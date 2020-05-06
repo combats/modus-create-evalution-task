2b88f63e3a7c7829950dd3ee15b3d472737087e4
Igor Tsinko <igor.tsinko@gmail.com>
2020-05-06 14:30:38 +0300

    basic config

---
+++ services/financial-balance/src/test/java/com/modus/create/financialbalance/FinancialBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modus.create.financialbalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialBalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
